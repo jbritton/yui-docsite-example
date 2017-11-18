000ad0b3231f70ffed9db1b9d74ef6f1b5e49511
Jeff Britton <britton.jeff@gmail.com>
2017-11-18 00:25:24 -0500

    Updates

---
+++ api.js
@@ -0,0 +1,52 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [],
+    "modules": [
+        "api",
+        "app",
+        "components",
+        "routes",
+        "services",
+        "store",
+        "views"
+    ],
+    "allModules": [
+        {
+            "displayName": "api",
+            "name": "api",
+            "description": "Provides restful services and model helpers."
+        },
+        {
+            "displayName": "app",
+            "name": "app",
+            "description": "The main entry point into the application"
+        },
+        {
+            "displayName": "components",
+            "name": "components",
+            "description": "Provides primitive design components like labels, buttons, icons, etc."
+        },
+        {
+            "displayName": "routes",
+            "name": "routes",
+            "description": "Provides routing configuration and middleware"
+        },
+        {
+            "displayName": "services",
+            "name": "services",
+            "description": "Provides application services"
+        },
+        {
+            "displayName": "store",
+            "name": "store",
+            "description": "Store for application data"
+        },
+        {
+            "displayName": "views",
+            "name": "views",
+            "description": "Application views"
+        }
+    ],
+    "elements": []
+} };
+});
